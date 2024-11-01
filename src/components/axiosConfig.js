993ab6bbd77c25aba6116cac73f6c7a207cc7370
Tary Junior <111458237+TN-Junior@users.noreply.github.com>
2024-11-01 09:47:15 -0300

    Delete src directory

--- src/components/axiosConfig.js
+++
@@ -1,8 +0,0 @@
-// src/axiosConfig.js
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api/auth',  // Base URL do backend Spring Boot
-});
-
-export default axiosInstance;
--- src/pages/loginPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Login from '../components/Login/login';
-
-const loginPage = () => {
-  return <Login />;
-};
-
-export default loginPage;
